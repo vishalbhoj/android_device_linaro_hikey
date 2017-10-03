080552e2a53fe07241276364c940a915a203ec23
John Stultz <john.stultz@linaro.org>
2017-10-03 14:24:18 -0700

    hikey/hikey960: Enable PRODUCT_FULL_TREBLE_OVERRIDE
    To start figuring out full-treble issues, enable
    the PRODUCT_FULL_TREBLE_OVERRIDE option.
    
    Change-Id: I0d732c6749ce29a03a4318a985d52b472f3aa81a
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- device-common.mk
+++ device-common.mk
@@ -17,6 +17,9 @@
 # Adjust the dalvik heap to be appropriate for a tablet.
 $(call inherit-product-if-exists, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 
+# enable Treble
+PRODUCT_FULL_TREBLE_OVERRIDE := true
+
 # Set vendor kernel path
 PRODUCT_VENDOR_KERNEL_HEADERS := device/linaro/hikey/kernel-headers
 
